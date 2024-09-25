c876715b525b2c93b3148e2a9715ac74d108114b
AutumnCod <109141163+AutumnsCode@users.noreply.github.com>
2024-09-25 11:06:02 +0200

    create the layout of the right sidebar for the homepage and the card layout without any real data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,34 @@
-import { HeaderBox } from '@/components/header-box'
+import { HeaderBox } from '@/components/header-box'; import { RightSidebar } from '@/components/right-sidebar'
 import { TotalBalanceBox } from '@/components/total-balance-box'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: 'Sarah'}
+  const loggedIn = {firstName: 'Sarah', lastName: 'Pruene', email: 'dummy@gmail.com'}
   return (
-    <section className="home">
-      <div className="home-content">
-        <header className="home-header">
-          <HeaderBox
-            type='greeting'
-            title='Welcone'
-            user={loggedIn?.firstName || "Guest"}
-            subtext='Access and manage your account and transactions efficiently.'
-          />
-        </header>
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
-        />
-      </div>
-    </section>
-  )
+		<section className='home'>
+			<div className='home-content'>
+				<header className='home-header'>
+					<HeaderBox
+						type='greeting'
+						title='Welcone'
+						user={loggedIn?.firstName || 'Guest'}
+						subtext='Access and manage your account and transactions efficiently.'
+					/>
+				</header>
+				<TotalBalanceBox
+					accounts={[]}
+					totalBanks={1}
+					totalCurrentBalance={1250.35}
+				/>
+			</div>
+
+			<RightSidebar
+				user={loggedIn}
+				transactions={[]}
+				banks={[{ currentBalance: 123.5 }, { currentBalance: 123.5 }]}
+			/>
+		</section>
+  );
 }
 
 export default Home
